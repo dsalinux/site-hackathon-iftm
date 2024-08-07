6cd5d23eabb2a6b7ba622516ec59653e45b1248c
Danilo Souza Almeida <dsalinux@gmail.com>
2024-08-07 19:26:20 -0300

    Novas Implementações

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ document.addEventListener("scroll", () => {
   console.log(window.scrollY > window.innerHeight * 0.5);
   if (window.scrollY > window.innerHeight * 0.5 && header.classList.contains('menu--alt') === false) {
     header.classList.add("menu--alt");
-    header.querySelector(".logo").style.position = "static";
-    header.querySelector(".logo").style.display = "flex";
+    // header.querySelector(".logo").style.position = "static";
+    // header.querySelector(".logo").style.display = "flex";
   } else if (window.scrollY < window.innerHeight * 0.5 && header.classList.contains('menu--alt')) {
     header.classList.remove("menu--alt");
-    header.querySelector(".logo").style.position = "absolute";
-    header.querySelector(".logo").style.display = "none";
+    // header.querySelector(".logo").style.position = "absolute";
+    // header.querySelector(".logo").style.display = "none";
   }
 });
